191dfee1dbb63ef816cd2c24c47cbfbda8792cb7
JB OLIVIER <jb.olivier57@outlook.fr>
2024-11-20 14:34:54 +0000

    maj fin branch INIT TEST

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// ./eslint.config.mjs
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+);
--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
+  testMatch: ["**/?(*.)+(sepc|test).ts*"],
 };
